fa775b8114c735851dbc0f443e0e3db50d6ab476
psvri <psvrishabh@gmail.com>
2023-06-25 13:57:19 +0530

    Adding vqsort benchmarks

--- benches/sort_benchmark.rs
+++ benches/sort_benchmark.rs
@@ -1,40 +1,71 @@
 #![cfg_attr(feature = "nightly", feature(portable_simd))]
 
+use std::fmt::Debug;
+
 use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
 use rand::{distributions::Uniform, Rng};
-use simd_sort::{self, };
+use simd_sort;
 
-pub fn create_uniform_data() -> Vec<u64> {
-    let mut rng = rand::thread_rng();
-    let range = Uniform::new(0, 1024 * 1024);
-    let vals: Vec<u64> = (0..1024 * 1024).map(|_| rng.sample(&range)).collect();
-    vals
-}
-
-pub fn create_uniform_data_i64() -> Vec<i64> {
-    let mut rng = rand::thread_rng();
-    let range = Uniform::new(0, 1024 * 1024);
-    let vals: Vec<i64> = (0..1024 * 1024).map(|_| rng.sample(&range)).collect();
+pub fn create_uniform_data<T>(size: usize) -> Vec<T>
+where
+    T: TryFrom<usize> + Default,
+    <T as TryFrom<usize>>::Error: Debug,
+{
+    let std_rng = rand::SeedableRng::seed_from_u64(10);
+    let mut rng = rand::rngs::StdRng::from(std_rng);
+    let range = Uniform::new(0, size);
+    let vals: Vec<T> = (0..size)
+        .map(|_| rng.sample(&range).try_into().unwrap_or_default())
+        .collect();
     vals
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let data = create_uniform_data_i64();
-    let data1 = data.clone();
+    let data = create_uniform_data::<i64>(1024 * 1024);
 
-    c.bench_function("std_unstable_sort", move |b| {
-        // This will avoid timing the to_vec call.
-        b.iter_batched(
-            || data1.clone(),
-            |mut data| {
-                data.sort_unstable();
-                black_box(data);
-            },
-            BatchSize::LargeInput,
-        )
-    });
+    #[cfg(feature = "rust_std_unstable")]
+    {
+        let data_t = data.clone();
+        c.bench_function("std_unstable_sort", move |b| {
+            // This will avoid timing the to_vec call.
+            b.iter_batched(
+                || data_t.clone(),
+                |mut data| {
+                    data.sort_unstable();
+                    black_box(data);
+                },
+                BatchSize::LargeInput,
+            )
+        });
+    }
 
-    #[cfg(target_feature = "avx2")]
+    #[cfg(any(feature = "cpp_vqsort", feature = "cpp_vqsort_avx2"))]
+    {
+        extern "C" {
+            fn vqsort_i64(data: *mut i64, size: usize);
+        }
+        unsafe {
+            let mut temp1 = data.clone();
+            let mut temp2 = data.clone();
+            vqsort_i64(temp1.as_mut_ptr(), temp1.len());
+            temp2.sort();
+            assert_eq!(temp1, temp2);
+            let data_t = data.clone();
+            c.bench_function("cpp_vqsort", move |b| {
+                // This will avoid timing the to_vec call.
+                b.iter_batched(
+                    || data_t.clone(),
+                    |mut data| {
+                        vqsort_i64(data.as_mut_ptr(), data.len());
+                        black_box(data);
+                    },
+                    BatchSize::LargeInput,
+                )
+            });
+        }
+    }
+
+    #[cfg(all(target_feature = "avx2", target_arch = "x86_64"))]
     {
         use simd_sort::platform::x86::avx2::avx2_sort_i64;
         let data_t = data.clone();
@@ -50,8 +81,8 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             )
         });
     }
-    
-    #[cfg(target_feature = "avx512f")]
+
+    #[cfg(all(target_feature = "avx512f", target_arch = "x86_64"))]
     {
         use simd_sort::platform::x86::avx512::avx512_sort_i64;
         let data_t = data.clone();
---
+++ build.rs
@@ -0,0 +1,112 @@
+#![allow(dead_code)]
+
+use std::{env, path::PathBuf};
+
+static CLANG_PATH: &'static str = "clang++-15";
+
+fn get_manifest_dir_path() -> PathBuf {
+    PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
+}
+
+fn build_and_link_cpp_sort(
+    file_name: &str,
+    specialize_fn: Option<fn(&mut cc::Build) -> Option<String>>,
+) {
+    let file_path = get_manifest_dir_path()
+        .join("other_implementations/")
+        .join(format!("{file_name}.cpp"));
+
+    // Tell Cargo that if the given file changes, to rerun this build script.
+    println!("cargo:rerun-if-changed={}", file_path.display());
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let mut builder = cc::Build::new();
+
+    builder
+        .file(file_path)
+        .cpp(true)
+        .warnings(false) // The thirdparties just have too many.
+        .flag_if_supported("/EHsc")
+        .flag_if_supported("/Zc:__cplusplus")
+        .flag_if_supported("/std:c++20")
+        .flag_if_supported("-std=c++20")
+        .flag_if_supported("-fdiagnostics-color=always")
+        .force_frame_pointer(false)
+        .define("NDEBUG", None)
+        .define("HWY_LIBRARY_TYPE", "STATIC")
+        .debug(false)
+        .opt_level(3);
+
+    let mut artifact_name = file_name.to_string();
+    if let Some(spec_fn) = specialize_fn {
+        if let Some(artifact_name_override) = spec_fn(&mut builder) {
+            artifact_name = artifact_name_override;
+        }
+    }
+    builder.compile(&artifact_name);
+
+    println!("{:?} {:?}", out_dir.display(), artifact_name);
+    println!("cargo:rustc-link-search={}", out_dir.display());
+    println!("cargo:rustc-link-lib=static={}", artifact_name);
+}
+
+#[cfg(all(feature = "cpp_vqsort", target_arch = "aarch64"))]
+fn build_and_link_cpp_vqsort() {
+    build_and_link_cpp_sort(
+        "cpp_vqsort",
+        Some(|builder: &mut cc::Build| {
+            builder.flag("-march=native+aes");
+            builder.include(
+                get_manifest_dir_path()
+                    .join("other_implementations")
+                    .join("vqsort"),
+            );
+            builder.compiler(CLANG_PATH);
+
+            None
+        }),
+    );
+}
+
+#[cfg(all(feature = "cpp_vqsort", target_arch = "x86_64"))]
+fn build_and_link_cpp_vqsort() {
+    build_and_link_cpp_sort(
+        "cpp_vqsort",
+        Some(|builder: &mut cc::Build| {
+            builder.flag("-march=native");
+            builder.include(
+                get_manifest_dir_path()
+                    .join("other_implementations")
+                    .join("vqsort"),
+            );
+            builder.compiler(CLANG_PATH);
+
+            None
+        }),
+    );
+}
+
+#[cfg(all(feature = "cpp_vqsort_avx2", target_arch = "x86_64"))]
+fn build_and_link_cpp_vqsort_avx2() {
+    build_and_link_cpp_sort(
+        "cpp_vqsort",
+        Some(|builder: &mut cc::Build| {
+            builder.flag("-march=x86-64-v3");
+            builder.include(get_manifest_dir_path().join("other_implementations/vqsort"));
+            builder.compiler(CLANG_PATH);
+            None
+        }),
+    );
+}
+
+#[cfg(not(feature = "cpp_vqsort"))]
+fn build_and_link_cpp_vqsort() {}
+
+#[cfg(not(feature = "cpp_vqsort_avx2"))]
+fn build_and_link_cpp_vqsort_avx2() {}
+
+fn main() {
+    build_and_link_cpp_vqsort();
+    build_and_link_cpp_vqsort_avx2();
+}
